83191aa1f732a0dbad4311f360e3f5aa83ff9b3d
peterstaal <peter@ctrllearning.nl>
2024-11-05 18:42:38 +0100

    navigatie toegevoegd aan app.vue

--- vuejs-masterclass-2024/src/App.vue
+++ vuejs-masterclass-2024/src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <input
+        type="text"
+        placeholder="Search..."
+        class="w-full pl-8 bg-background"
+      />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
