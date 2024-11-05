6043e080a4b28f5b2e9d7f741b4ef1ca9c05a402
peterstaal <peter@ctrllearning.nl>
2024-11-05 18:26:59 +0100

    shadcn toegevoegd voor components

--- vuejs-masterclass-2024/vite.config.ts
+++ vuejs-masterclass-2024/vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
 import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+
+import tailwind from 'tailwindcss'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [VueRouter(), vue()],
   resolve: {
     alias: {
