b0d947aa893b2ad4682f10c677f927e9bd8186e6
peterstaal <peter@ctrllearning.nl>
2024-11-05 12:05:26 +0100

    filebased routing opgezet

--- vuejs-masterclass-2024/src/router/index.ts
+++ vuejs-masterclass-2024/src/router/index.ts
@@ -1,31 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'notFound',
-      component: h('p', { style: 'color: red;' }, '404 Not Found'),
-    },
-  ],
+  routes,
 })
 
 export default router
