41b993e8b41f51049fd804515795d46795f9a175
peterstaal <peter@ctrllearning.nl>
2024-11-18 15:38:09 +0100

    fetchen van projecten verplaatst naar een projectsstore

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,18 +2,14 @@
 import { projectsQuery } from '@/utils/supaQueries'
 import { columns } from '@/utils/tableColumns/projectsColumns'
 import type { Projects } from '@/utils/supaQueries'
+import {useProjectsStore} from "@/stores/loaders/projects";
+import {storeToRefs} from "pinia";
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Projects | null>(null)
-const getProjects = async () => {
-  const { data, error, status } = await projectsQuery
-
-  if (error) useErrorStore().setError({ error, customCode: status })
-
-  projects.value = data
-}
-
+const projectsLoader = useProjectsStore()
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
 await getProjects()
 </script>
 
