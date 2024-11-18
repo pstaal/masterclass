27a1c3bc937a6db2fc480eb260acbaf381986bdf
peterstaal <peter@ctrllearning.nl>
2024-11-18 16:05:04 +0100

    validate cache functie toegevoegd

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -5,13 +5,28 @@ import type { Projects } from "@/utils/supaQueries";
 export const useProjectsStore = defineStore('projects-store', () => {
     const projects = ref<Projects | null>(null)
     const loadProjects = useMemoize(async (key:string) => await projectsQuery)
-    const getProjects = async () => {
 
+    const validateCache = () => {
+        if (projects.value?.length) {
+            projectsQuery.then(({data}) => {
+                if (JSON.stringify(projects.value) === JSON.stringify(data)){
+                    console.log('Cached and fresh data matched')
+                    return;
+                } else {
+                    console.log('something has changed')
+                    loadProjects.delete('projects')
+                }
+            })
+        }
+    }
+    const getProjects = async () => {
         const { data, error, status } = await loadProjects('projects')
 
         if (error) useErrorStore().setError({ error, customCode: status })
 
         projects.value = data
+
+        validateCache()
     }
 
     return {
